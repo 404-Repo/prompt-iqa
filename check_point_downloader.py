5e5b0932f66903b3d2fb4d082eb61c25812bb04c
tesha <tereshin.tesha@gmail.com>
2024-12-18 18:18:02 +0000

    [UPDATED] fixed installation issues

--- check_point_downloader.py
+++ check_point_downloader.py
@@ -3,9 +3,13 @@ from pathlib import Path
 from huggingface_hub import snapshot_download
 
 
-if __name__ == '__main__':
+def main():
     checkpoint_dir_default = Path(os.path.join(os.curdir, "./PromptIQA/checkpoints"))
     if not checkpoint_dir_default.exists():
         checkpoint_dir_default.mkdir(parents=True, exist_ok=True)
 
     snapshot_download(repo_id='Zevin2023/PromptIQA', local_dir=checkpoint_dir_default)
+
+
+if __name__ == '__main__':
+    main()
